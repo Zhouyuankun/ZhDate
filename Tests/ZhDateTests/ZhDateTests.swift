55a0526dd16442f693cd312eca691b5a6ccc3409
Zhouyuankun <zykun7525599>
2023-06-07 20:29:51 +0800

    First commit

--- Tests/ZhDateTests/ZhDateTests.swift
+++ Tests/ZhDateTests/ZhDateTests.swift
@@ -6,6 +6,6 @@ final class ZhDateTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ZhDate().text, "Hello, World!")
+        //XCTAssertEqual(ZhDate().text, "Hello, World!")
     }
 }
